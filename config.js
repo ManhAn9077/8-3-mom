f283a9eff4bd5a7bdc30c56e035c888c3aaf13eb
ManhAn9077 <94302143+ManhAn9077@users.noreply.github.com>
2022-03-08 11:01:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mom';
+const giftUrl = 'https://www.tiktok.com/@anhdung8890/video/6936142236476345601?is_copy_url=1&is_from_webapp=v1&q=8%2F3&t=1646704928405';
+const eventName = 'Chúc Mừng 08/03';
+const titleCard = '8/3 ý nghĩa';
+const contentCard = 'Nay 8/3 con có đôi lời muốn gửi tới mẹ. Con chúc mẹ luôn mạnh khỏe, vui vẻ, hạnh phúc. Chúc mẹ buôn bán thuận lợi để có tiền nuôi bọn con khôn lớn. Yêu mẹ';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'mom.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
